ad82c6a6a73348935b4a8309ce69b12ca0c25615
Jake Dykstra <jd0523@gmail.com>
2019-12-02 22:29:45 -0800

    application runs with ts in react

---
+++ client/src/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const Hello: React.FunctionComponent<{ compiler: string, framework: string }> = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <div>{props.compiler}</div>
+      <div>{props.framework}</div>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <Hello compiler="TypeScript" framework="React" />,
+  document.getElementById("root")
+);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,37 +1,45 @@
 var path = require('path');
-var HtmlWebpackPlugin =  require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin =  require('html-webpack-plugin');
 
 module.exports = {
-    entry : './app/index.js',
-    output : {
-        path : path.resolve(__dirname , 'dist'),
-        filename: 'index_bundle.js'
-    },
-    module : {
-        rules : [
-            {test : /\.(js)$/, use:'babel-loader'},
-            {test : /\.css$/, use:['style-loader', 'css-loader']},
-            {
-                test: /\.less$/,
-                use: [
-                  {
-                    loader: 'style-loader', // creates style nodes from JS strings
-                  },
-                  {
-                    loader: 'css-loader', // translates CSS into CommonJS
-                  },
-                  {
-                    loader: 'less-loader', // compiles Less to CSS
-                  },
-                ],
-              },
-        ]
-    },
-    mode:'development',
-    plugins : [
-        new HtmlWebpackPlugin ({
-            template : 'app/index.html'
-        })
-    ]
+  entry : './src/index.tsx',
+  plugins : [
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ['public/build']
+    }),
+    new HtmlWebpackPlugin ({
+      template : './public/index.html'
+    })
+  ],
+  output : {
+    path : path.resolve(__dirname , '/public'),
+    filename: 'build/[name].[contenthash].js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module : {
+      rules : [
+          {test : /\.(js)$/, use:'babel-loader'},
+          {test : /\.css$/, use:['style-loader', 'css-loader']},
+    {
+              test: /\.less$/,
+              use: [
+                {
+                  loader: 'style-loader', // creates style nodes from JS strings
+                },
+        {
+                  loader: 'css-loader', // translates CSS into CommonJS
+        },
+                {
+                  loader: 'less-loader', // compiles Less to CSS
+                },
+              ],
+            },
+            { test: /\.tsx?$/, loader: 'ts-loader' }
+      ]
+  },
+    // mode:'development',
 
 }
